bb6237f04636ba674d533c4eaf25e86e4aab2c59
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-01-25 23:55:49 -0500

    pop_first

--- linkedlist.py
+++ linkedlist.py
@@ -55,4 +55,13 @@ class LinkedList:
         self.length += 1
         return True
     
-
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -=1
+        if self.length == 0:
+            self.tail = None
+        return temp
