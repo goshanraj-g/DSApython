e579909f654730305c71fb5b8dbffdef7e0bdb58
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-01-27 22:34:51 -0500

    remove

--- linkedlist.py
+++ linkedlist.py
@@ -96,3 +96,18 @@ class LinkedList:
         temp.next = new_node
         self.length +=1
         return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+        
