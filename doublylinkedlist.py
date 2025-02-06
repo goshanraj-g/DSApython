0dd50dcfcfbc9fbc4331bb6518ec136364953c1c
Goshanraj Govindaraj <govindag@mcmaster.ca>
2025-02-06 15:34:05 -0500

    initializedll

--- doublylinkedlist.py
+++ doublylinkedlist.py
@@ -3,3 +3,10 @@ class Node:
         self.value = value
         self.next = None
         self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
